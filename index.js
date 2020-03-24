ce96091c4c8f9a6bcba916919c0d07cc0b8a829a
cfprice3 <cfprice3@gmail.com>
2020-03-24 10:51:06 -0400

    created variables to assign answers to, created a variable for markdown for styling, created writeFile function.

--- index.js
+++ index.js
@@ -71,11 +71,38 @@ questions
 
 // Function to generate answers
 .then(function (answers) {
-    console.log(answers)
+    // console.log(answers)
 
 // Axios API call to github using user name from prompt input    
     axios.get(`https://api.github.com/users/${answers.username}`)
         .then(function (response) {
             // console.log(response.data);
+
+// Declaring variables and their values to generate answers           
+            let name = response.data.name;
+            let profileImage = response.data.avatar_url;
+            let gitUrl = response.data.html_url;
+            let username = `${answers.username}`;
+            let email= `${answers.email}`;
+            let projName = `${answers.projName}`
+            let description = `${answers.description}`;
+            let test = `${answers.test}`;
+            let contrib= `${answers.contrib}`;
+            let install = `${answers.install}`;
+            let usage = `${answers.usage}`;
+            let badge = `![travis build](https://img.shields.io/travis/${answers.username}/${answers.projName}.svg)`
+            let license = '["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]'
+            let Table = `1. Installation\n 2. Usage\n 3. License\n 4. Contributing\n 5. Tests\n 6. Questions`;
+// Testing some of the variables in console
+            // console.log(test, profileImage, username, name, gitUrl, email, projName, description, contrib, install)
+            
+            
+// Declaring a variable containing all the generated answers, and styling it in markdown format            
+            var markdown = `${badge}\n# **${projName}**\n\n#### *Created By ${name}, (${username})*\n#### email: ${email}\n#### git url: ${gitUrl}\n![profileavatar](${profileImage})\n\n### *Project Description:*\n${description}\n\n\n\n ## **Table of Contents**\n ${Table}\n\n\n\n### **1. Installations:**\n${install}\n\n\n### **2. Usage:**\n${usage}\n\n\n### **3. License:**\n${license}\n\n\n### **4. How you can contribute:**\n${contrib}\n\n\n### **5. Tests:**\n${test}\n\n\n### **6. Questions:**\nPlease feel free to ask or add any questions!`
+
+// Calling fs and writeFile function to create a readme.md file with the generated template and answers.
+            fs.writeFile('README.md', markdown, function (err) {
+                if (err) console.log(err)
+            });
         });
     });
