c058604f6be672bc0ce205295d7814515cf09dd7
cfprice3 <cfprice3@gmail.com>
2020-03-24 10:04:12 -0400

    added prompts to input questions, added notes

--- index.js
+++ index.js
@@ -9,44 +9,68 @@ const questions = require("inquirer");
 
 // prompts requiring user input to populate later variables
 questions
+
+// Github User Name
   .prompt([{
     type: "input",
     message: "Enter your GitHub username:",
     name: "username"
   },
 
+// Email
   {
     type: "input",
     message: "What is your e-mail:",
     name: "email"
   },
 
+// Project name 
   {
     type: "input",
     message: "What is your Project name:",
     name: "projName"
   },
 
+// Project description
   {
     type: "input",
     message: "Describe your Project:",
     name: "description"
   },
 
+// Tests  
   {
     type: "input",
-    message: "Explain how to run Tests for project",
+    message: "Explain how to run Tests for project:",
     name: "test"
   },
 
+// Contributions  
   {
     type: "input",
-    message: "Explain how to Contribute to the project",
+    message: "Explain how to Contribute to the project:",
     name: "contrib"
-  }
-])
+  },
 
-//Still need Table of Contents, Installation, Usage, 
+// Installations  
+  {
+    type: "input",
+    message: "Explain what installations are needed and how to install them:",
+    name: "install"
+  },
+
+// Usage  
+  {
+    type: "input",
+    message: "Explain the usage of the project:",
+    name: "usage"
+  }
+
+])
+//  End of prompted questions 
+
+
+//Still need Table of Contents 
 // License ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"], Questions, Instructions
 
 
