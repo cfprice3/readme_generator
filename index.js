2790e027269fa107fdf13fd4345d2e4458a33e6e
cfprice3 <cfprice3@gmail.com>
2020-03-24 10:24:46 -0400

    changed axios api call into the index.js, instead of separate folder.

--- index.js
+++ index.js
@@ -69,26 +69,13 @@ questions
 ])
 //  End of prompted questions 
 
+// Function to generate answers
+.then(function (answers) {
+    console.log(answers)
 
-//Still need Table of Contents 
-// License ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"], Questions, Instructions
-
-
-.then(function({ username }) {
-    const api2 = require("./api.js");
-    const name = api2.getUser(`${username}`);
-    axios.get(name)
-    .then(data => {
-        console.log(data.data.login);
-        console.log(data.data.avatar_url);
-        console.log(data.data.email);
-
-        const getResults = [
-            `Project name: ${projName}"\n"
-             GitHub Name: ${data.data.login},
-             ${data.data.avatar_url}
-             Email: ${this.email}`
-       ];
-       console.log(getResults)
-    })
-});
+// Axios API call to github using user name from prompt input    
+    axios.get(`https://api.github.com/users/${answers.username}`)
+        .then(function (response) {
+            // console.log(response.data);
+        });
+    });
